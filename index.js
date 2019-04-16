d195734394d3e986f3405da3fdc9fe62966d075a
Souvik Das <souvik131@gmail.com>
2019-04-16 10:41:48 +0530

    msg

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let nlp={
 					},
 	vsm:(tokens,dict)=>dict.map((token)=>tokens.reduce((acc,curr)=>curr == token?acc +1:acc,0)),
 	tf:(vsm,numberOfTokens)=>vsm.map((token)=>token/numberOfTokens),
-	idf:(documentTokens,dict)=>dict.map((word)=>Math.log(2/documentTokens.reduce((acc,curr)=>curr.includes(word)?1:0+acc,0))),
+	idf:(documentTokens,dict)=>dict.map((word)=>Math.log(documentTokens.length/documentTokens.reduce((acc,curr)=>curr.includes(word)?1:0+acc,0))),
 	tfidf:(tf,idf)=>tf.map((element,index)=>element*idf[index]),
 	cosine:(tfIdf1,tfIdf2)=>tfIdf1.reduce((acc,curr,index)=>acc+curr*tfIdf2[index],0) / (Math.sqrt(tfIdf1.reduce((acc,curr)=>acc+curr*curr,0))*Math.sqrt(tfIdf2.reduce((acc,curr)=>acc+curr*curr,0)))
 }
